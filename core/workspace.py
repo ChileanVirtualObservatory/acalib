b922adcc10f233c7b42b6c46a8598ba89918dbc1
Mauricio Araya <maray@inf.utfsm.cl>
2015-06-15 10:54:45 -0300

    Fixed gui
    Former-commit-id: a3cd465b5aaffabb5f3cf93d3c57c54b5a60675c

--- core/workspace.py
+++ core/workspace.py
@@ -69,24 +69,24 @@ def import_file(path,ws=_ws_df):
    else:
       _ascii_consumer(path,name,ws)
 
-#def real_dims(ndd):
-#   shape=[]
-#   dim=0
-#
-#   for i in range(ndd.data.ndim):
-#      if ndd.data.shape[i] != 1:
-#         shape.append(ndd.data.shape[i])
-#         dim+=1
-#   if dim==1:
-#      otype="Spectra"
-#   elif dim==2:
-#      otype="Image"
-#   elif dim>=3:
-#      otype="Cube"
-#   else:
-#      log.warning("NDData of 0 dimension? ignoring...")
-#      return 
-#   return (dim,shape,otype)
+def real_dims(ndd):
+   shape=[]
+   dim=0
+
+   for i in range(ndd.data.ndim):
+      if ndd.data.shape[i] != 1:
+         shape.append(ndd.data.shape[i])
+         dim+=1
+   if dim==1:
+      otype="Spectra"
+   elif dim==2:
+      otype="Image"
+   elif dim>=3:
+      otype="Cube"
+   else:
+      log.warning("NDData of 0 dimension? ignoring...")
+      return 
+   return (dim,shape,otype)
 
 
 #def declare_metadata(client):
