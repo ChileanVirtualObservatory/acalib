99f5a5985e99cbc3662b114787d282df42dc363a
Mauricio Araya <maray@inf.utfsm.cl>
2015-09-28 15:18:08 -0300

    Added some tests
    Former-commit-id: 63a1e8080e5ac07737156d334c842c4fd18bc818

---
+++ test/test_simple.py
@@ -0,0 +1,31 @@
+import core.workspace as ws
+import numpy as np
+import matplotlib.pyplot as plt
+import timeit
+import cProfile
+import clumps.simpleClumps as sclumps
+import matplotlib.pyplot as plt
+
+ws.import_file("fits/M100line.image.fits")
+#ws.import_file("fits/Orion.methanol.cbc.contsub.image.fits")
+
+elm=ws.elements()
+cube=elm['M100line.image-0']
+#cube=elm['Orion.methanol.cbc.contsub.image-0']
+spar=cube.standarize()
+sc=sclumps.SimpleClumps()
+print "spar",spar
+
+# Fitme :S
+clist=sc.fit(cube,verbose=True)
+plt.subplot(1, 3, 1)
+plt.imshow(cube.get_stacked())
+plt.subplot(1, 3, 2)
+plt.imshow(sc.data.get_stacked())
+plt.subplot(1, 3, 3)
+plt.imshow(sc.syn.get_stacked())
+plt.show()
+
+
+
+
