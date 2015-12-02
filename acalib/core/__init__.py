a59210c3bb991cf2b1a60b3f5cea537d6ab975ab
Camilo Andres Valenzuela Carrasco <cacke91@gmail.com>
2015-12-02 01:35:05 -0300

    Added table to container
    Former-commit-id: b966cb6441fba2b273674715ac7cef662f6ca1aa

--- acalib/core/__init__.py
+++ acalib/core/__init__.py
@@ -1,8 +1,10 @@
 #Importing Core 
 
 from . import data
+from . import atable
 from . import flux
 from . import parameter
+from .atable import *
 from .data import *
 from .flux import *
 from .parameter import *
