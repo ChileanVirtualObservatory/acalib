0f2af9456b2695fa26a899978dc1fa932ff7c200
Mauricio Araya <maray@inf.utfsm.cl>
2016-01-10 12:52:29 -0300

    Added a test
    Former-commit-id: 61ef12ba6900793622c654609d04178610f536de

---
+++ test/test_bubble_cluster.py
@@ -0,0 +1,42 @@
+import acalib.core.workspace as ws
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import timeit
+import cProfile
+import acalib.clumps.bubbleClumps as cl
+import matplotlib.pyplot as plt
+
+ws.import_file("fits/M100line.image.fits")
+#ws.import_file("fits/Orion.methanol.cbc.contsub.image.fits")
+#ws.import_file("fits/Antennae_North.CO3_2Line.Clean.pcal1.image.fits")
+#ws.import_file("fits/Antennae_South.CO3_2Line.Clean.pcal1.image.fits")
+#ws.import_file("fits/CenA.CO2_1Line.Clean.image.fits")
+
+elm=ws.elements()
+cube=elm['M100line.image-0']
+#cube=elm['Orion.methanol.cbc.contsub.image-0']
+#cube=elm['Antennae_North.CO3_2Line.Clean.pcal1.image-0']
+#cube=elm['Antennae_South.CO3_2Line.Clean.pcal1.image-0']
+#cube=elm['CenA.CO2_1Line.Clean.image-0']
+spar=cube.standarize()
+gc=cl.BubbleClumps()
+# use_meta not implemented yet, so compute parameters to use
+pixbsize=cube.meta['BMIN']/abs(cube.meta['CDELT1'])
+print "beam size in pixels =",pixbsize
+gc.par['FWHMBEAM']=pixbsize
+
+total=cube.sum()
+telem=float(cube.count())
+maxporc=0.001
+samples=maxporc*telem
+print "Maximum Bubbles",int(samples),"= ",maxporc*100,"%" 
+gc.par['MAXBUB']=int(samples)
+snrlimit=1.0
+print "SNR = ",snrlimit
+gc.par['SNRLIMIT']=snrlimit
+gc.fit(cube,verbose=True)
+gc.clusterize()
+plt.imshow(gc.syn.get_stacked())
+plt.show()
+
