73485b491a3f7eadb15bb43faf4751e953611b73
Camilo Valenzuela <cacke91@gmail.com>
2016-05-05 16:26:46 -0300

    Added UTFSM Indexing
    Former-commit-id: 15b3672eb5ec101ca3fed5425321f79737d56615

--- test/roi/test_indexing.py
+++ test/roi/test_indexing.py
@@ -9,6 +9,6 @@ c = acalib.AContainer()
 io.load_to_cont('/home/cvalenzu/Escritorio/indexing_utfsm/ALMA00000085.fits',c)
 cube = c.primary
 rd = ri.RoiDetect()
-spectra = rd.cube_spectra(cube,200)
+#spectra = rd.cube_spectra(cube,200)
 
 h1 = rd.vel_stacking(cube,67,96)
