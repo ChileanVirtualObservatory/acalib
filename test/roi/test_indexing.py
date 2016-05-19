363aa39d43f7884e3edce39ce7ff3212d79c06ae
Camilo Valenzuela <cacke91@gmail.com>
2016-05-19 00:59:54 -0400

    First attempt Gaussian Multiscale Segmentation
    Former-commit-id: ae9c9431281513d1598633e16d6ef11981503f27

--- test/roi/test_indexing.py
+++ test/roi/test_indexing.py
@@ -4,11 +4,46 @@ sys.path.append('../../')
 import acalib
 import acalib.io.formats as io
 import acalib.roi as ri
+import matplotlib.pyplot as plt
 
-c = acalib.AContainer()
-io.load_to_cont('/home/cvalenzu/Escritorio/indexing_utfsm/ALMA00000085.fits',c)
-cube = c.primary
+
+#c = acalib.AContainer()
+#io.load_to_cont('/home/cvalenzu/Escritorio/indexing_utfsm/ALMA00000085.fits',c)
+#cube = c.primary
 rd = ri.RoiDetect()
-#spectra = rd.cube_spectra(cube,200)
 
-h1 = rd.vel_stacking(cube,67,96)
+
+#print "Sketching Cube Spectra"
+#spectra = rd.cube_spectra(cube,50)
+
+#print "Plotting"
+#frecs = range(950)
+#plt.bar(frecs, spectra)
+#plt.show()
+
+#h1 = rd.vel_stacking(cube,0,30)
+
+#h1t = h1.T
+#plt.imshow(h1t, origin='lower')
+#plt.show()
+
+#rd.gaussian_mix(h1)
+from astropy.io import fits
+
+
+rd = ri.RoiDetect()
+hdulist = fits.open('/home/cvalenzu/Escritorio/indexing_utfsm/drC-crop.fits')
+ndd = hdulist[0]
+data = ndd.data
+
+fts = rd.gaussian_mix(data)
+
+#import numpy as np
+#from skimage.filter import threshold_adaptive
+#image = data.astype(float)
+
+#f = (image-np.min(image))/(np.max(image)-np.min(image))
+#g = threshold_adaptive(f,360,method='mean',offset=0)
+
+#plt.imshow(g, origin='lower')
+#plt.show()
