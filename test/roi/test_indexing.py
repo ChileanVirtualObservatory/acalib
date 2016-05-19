83b650f9c30d0ac801767e1deb7f871a5581c15e
Camilo Valenzuela <cacke91@gmail.com>
2016-05-19 01:38:51 -0400

    Fixed kernelsmooth
    Former-commit-id: 403f816acaa3d98995b33cda5ed6e0df140ab619

--- test/roi/test_indexing.py
+++ test/roi/test_indexing.py
@@ -7,38 +7,41 @@ import acalib.roi as ri
 import matplotlib.pyplot as plt
 
 
-#c = acalib.AContainer()
-#io.load_to_cont('/home/cvalenzu/Escritorio/indexing_utfsm/ALMA00000085.fits',c)
-#cube = c.primary
-rd = ri.RoiDetect()
-
-
-#print "Sketching Cube Spectra"
-#spectra = rd.cube_spectra(cube,50)
-
-#print "Plotting"
-#frecs = range(950)
-#plt.bar(frecs, spectra)
-#plt.show()
-
-#h1 = rd.vel_stacking(cube,0,30)
-
-#h1t = h1.T
-#plt.imshow(h1t, origin='lower')
-#plt.show()
-
-#rd.gaussian_mix(h1)
-from astropy.io import fits
-
+import numpy as np
 
 rd = ri.RoiDetect()
-hdulist = fits.open('/home/cvalenzu/Escritorio/indexing_utfsm/drC-crop.fits')
-ndd = hdulist[0]
-data = ndd.data
 
-fts = rd.gaussian_mix(data)
+c = acalib.AContainer()
+io.load_to_cont('/home/cvalenzu/Escritorio/indexing_utfsm/ALMA00000085.fits',c)
+cube = c.primary
 
-#import numpy as np
+
+print "Sketching Cube Spectra"
+spectra = rd.cube_spectra(cube,196)
+
+print "Plotting"
+frecs = range(950)
+plt.bar(frecs, spectra)
+plt.show()
+
+h1 = rd.vel_stacking(cube,0,30)
+
+h1t = h1.T
+plt.imshow(h1t, origin='lower')
+plt.show()
+
+rd.gaussian_mix(h1)
+
+
+#from astropy.io import fits
+
+#rd = ri.RoiDetect()
+#hdulist = fits.open('/home/cvalenzu/Escritorio/indexing_utfsm/drC-crop.fits')
+#ndd = hdulist[0]
+#data = ndd.data
+
+#fts = rd.gaussian_mix(data)
+
 #from skimage.filter import threshold_adaptive
 #image = data.astype(float)
 
@@ -47,3 +50,9 @@ fts = rd.gaussian_mix(data)
 
 #plt.imshow(g, origin='lower')
 #plt.show()
+
+
+#x = np.arange(1,17).reshape((4,4)).astype(float)
+#kern = np.ones((4,4)) * 0.01
+
+#print rd._kernelsmooth(x,kern)
