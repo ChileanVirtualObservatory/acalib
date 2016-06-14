a593cbcfcc2ac3da8846f233b1230d51f0e4985d
mavillan <mavillan@alumnos.inf.utfsm.cl>
2016-06-13 23:37:07 -0400

    1)Improved fellwalker functionality.
    Former-commit-id: 678f9d76730fdb4879fd4af4c76a53f3a889cace

--- test/clumping/test_fellwalker.py
+++ test/clumping/test_fellwalker.py
@@ -3,11 +3,10 @@ sys.path.append('../../')
 
 import acalib.io.workspace as ws
 import numpy as np
-import matplotlib.pyplot as plt
-import timeit
-import cProfile
+#import timeit
+#import cProfile
 import acalib.clumps.fellWalker as fwalker
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from acalib import acontainer as ac
 #from acalib.io import graph as gp
 
@@ -20,10 +19,10 @@ spar=cube.standarize()
 
 fw = fwalker.FellWalker()
 
-caa = fw.fit(cube)
+caa,clumps = fw.run(cube, verbose=True)
 
-newcube=cube.copy()
-newcube.data=caa
+#newcube=cube.copy()
+#newcube.data=caa
 
 #gp.stacked(newcube)
 #gp.volume(newcube)
