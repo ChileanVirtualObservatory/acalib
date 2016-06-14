d300eeb5a0f0b51fbed978d3f6688ca1cc666a67
Mauricio Araya <mauricio@mac.local>
2016-06-14 18:53:13 -0300

    Still not compiling, but advancing
    Former-commit-id: 803af4d3a0609fa1e6417d96f838957ff4b5e347

--- acalib/cupid/wrappers/fitsio.h
+++ acalib/cupid/wrappers/fitsio.h
@@ -1,6 +1,9 @@
 #ifndef FITSIO_DEFINED
 #define FITSIO_DEFINED
 
+typedef struct fitsfile_t {
+
+} fitsfile;
 
 #endif  /* FITSIO_DEFINED */
 
--- acalib/cupid/wrappers/star/kaplibs.h
+++ acalib/cupid/wrappers/star/kaplibs.h
@@ -1,6 +1,10 @@
 #ifndef KAPLIBS_DEFINED
 #define KAPLIBS_DEFINED
+#include <stdio.h>
+#include "star/grp.h"
 
+
+void kpg1Rgndf( const char *, size_t, size_t, const char *, Grp **, size_t *, int * );
 
 #endif  /* CVG_DEFINED */
 
