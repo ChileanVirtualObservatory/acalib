059ac6983b02cf336ee89be0b9df3a38dc2e9f53
Mauricio Araya <mauricio@mac.local>
2016-06-15 02:10:07 -0300

    Filled a little bit the wrappers
    Former-commit-id: 3ddf154774587ab8db22b4f7acb0160faf552c94

--- acalib/cupid/wrappers/star/irq.h
+++ acalib/cupid/wrappers/star/irq.h
@@ -5,5 +5,12 @@ typedef struct IRQLocs_t {
 
 } IRQLocs;
 
+void irqDelet( int, int * );
+void irqNew( int, const char *, IRQLocs **, int * );
+void irqAddqn( const IRQLocs *, const char *, int, const char *, int * );
+void irqSetqm( const IRQLocs *, int, const char *, int, float *, int *, int * );
+void irqRlse( IRQLocs **, int * );
+
+
 #endif  /* IRQ_DEFINED */
 
