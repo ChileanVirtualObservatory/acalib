5dda707a3cb88ba0084b0f8b8f24f799643bf4f6
Manuel Sánchez <manuel.sanchez@linux.com>
2016-06-17 03:23:58 -0400

    Implemented index of max and min

--- acalib/core/indexes.py
+++ acalib/core/indexes.py
@@ -1,22 +1,57 @@
-from astropy.nddata import NDData
+import numpy as np
+from utils import ndslice, adjust_index
 
-def index_of_max(data, lower, upper):
+def index_of_max(ndd, lower=None, upper=None):
+    """ 
+    Index of maximum value in an m-dimensional subarray from 
+    an n-dimensional array.
+    
+    Arguments:
+        ndd   -- an astropy.nddata.NDDataArray object.
+        lower -- n-dimensional point as an n-tuple.
+        upper -- n-dimensional point as an n-tuple.
+    
+    Returns:
+        A tuple with the maximum value found in the m-dimensional
+        subarray and .
+        
+    """
+    ndd = ndslice(ndd, lower, upper)
+    index = np.unravel_index(ndd.data.argmax(), ndd.data.shape)
+    value = ndd.data[index]
+    return (value, adjust_index(index, lower))
+
+def index_of_min(ndd, lower=None, upper=None):
+    """ 
+    Index of maximum value in an m-dimensional subarray from 
+    an n-dimensional array.
+    
+    Arguments:
+        ndd   -- an astropy.nddata.NDDataArray object.
+        lower -- n-dimensional point as an n-tuple.
+        upper -- n-dimensional point as an n-tuple.
+    
+    Returns:
+        A tuple with the maximum value found in the m-dimensional
+        subarray and .
+        
+    """
+    ndd = ndslice(ndd, lower, upper)
+    index = np.unravel_index(ndd.data.argmin(), ndd.data.shape)
+    value = ndd.data[index]
+    return (value, adjust_index(index, lower))
+
+def index_to_world(ndd, index):
     pass
 
-def index_of_min(data, lower, upper):
+def index_as_features(ndd, lower, upper):
     pass
 
-def index_to_world(data, index):
+def index_from_window(ndd, wcs_center, wcs_window):
     pass
 
-def index_as_features(data, lower, upper):
+def index_fix(ndd, index):
     pass
 
-def index_from_window(data, wcs_center, wcs_window):
-    pass
-
-def index_fix(data, index):
-    pass
-
-def index_mesh(data):
+def index_mesh(ndd):
     pass
