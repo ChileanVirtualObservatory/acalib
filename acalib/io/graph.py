08c942394820c00c9631222051d94853ec7797b3
Mauricio Araya <mauricio@ip172.3.priv.inf.utfsm.cl>
2016-06-21 11:30:09 -0300

    First large refactoring
    Former-commit-id: 3d6ef37cff56b00cdb540865db2e915153ed23b0

--- acalib/io/graph.py
+++ acalib/io/graph.py
@@ -1,17 +1,16 @@
-from ..core import adata
-from ..core import atable
 from astropy import log
 import numpy as np
 from astropy.wcs import wcs
 from mayavi import mlab
+from ..core.utils import get_mesh,get_ranges
 
-def volume(cube):
+def volume(cube,wcs):
      figure = mlab.figure('Volume Plot')
-     xi, yi, zi = cube.get_mesh()
-     ranges=cube.get_ranges()
-     grid = mlab.pipeline.scalar_field(xi, yi, zi, cube.data)
-     mmin = cube.data.min()
-     mmax = cube.data.max()
+     xi,yi,zi=get_mesh(cube)
+     ranges=get_ranges(cube,wcs)
+     grid = mlab.pipeline.scalar_field(xi, yi, zi, cube)
+     mmin = cube.min()
+     mmax = cube.max()
      mlab.pipeline.volume(grid)#,vmin=mmin, vmax=mmin)
      ax=mlab.axes(xlabel="VEL [km/s] ",ylabel="DEC [deg]",zlabel="RA [deg]",ranges=ranges,nb_labels=5)
      ax.axes.label_format='%.3f'
@@ -19,48 +18,48 @@ def volume(cube):
      mlab.show()
 
 
-def contour(cube):
+def contour(cube,wcs):
      figure = mlab.figure('Contour Plot')
-     xi, yi, zi = cube.get_mesh()
-     ranges=cube.get_ranges()
+     xi,yi,zi=get_mesh(cube)
+     ranges=get_ranges(cube,wcs)
      mmin = cube.min()
      mmax = cube.max()
-     mlab.contour3d(xi,yi,zi,cube.data,transparent=True,contours=10,opacity=0.5)
+     mlab.contour3d(xi,yi,zi,cube,transparent=True,contours=10,opacity=0.5)
      ax=mlab.axes(xlabel="VEL [km/s] ",ylabel="DEC [deg]",zlabel="RA [deg]",ranges=ranges,nb_labels=5)
      ax.axes.label_format='%.3f'
      mlab.colorbar(title='flux', orientation='vertical', nb_labels=5)
      mlab.show()
 
-def stacked(cube):
-     figure = mlab.figure('Stacked Plot')
-     ranges=cube.get_ranges()
-     ranges=[ranges[2],ranges[3],ranges[4],ranges[5],ranges[0],ranges[1]]
-     img=cube.stack()
-     mlab.imshow(img)
-     ax=mlab.axes(xlabel="DEC [deg]",ylabel="RA [deg]",zlabel="VEL [km/s] ",ranges=ranges,nb_labels=5)
-     ax.axes.label_format='%.3f'
-     mlab.colorbar(title='flux', orientation='vertical', nb_labels=5)
-     mlab.show()
+#def stacked(cube):
+#     figure = mlab.figure('Stacked Plot')
+#     ranges=cube.get_ranges()
+#     ranges=[ranges[2],ranges[3],ranges[4],ranges[5],ranges[0],ranges[1]]
+#     img=cube.stack()
+#     mlab.imshow(img)
+#     ax=mlab.axes(xlabel="DEC [deg]",ylabel="RA [deg]",zlabel="VEL [km/s] ",ranges=ranges,nb_labels=5)
+#     ax.axes.label_format='%.3f'
+#     mlab.colorbar(title='flux', orientation='vertical', nb_labels=5)
+#     mlab.show()
 
 
-def velocity(cube):
-     figure = mlab.figure('Velocity Map')
-     ranges=cube.get_ranges()
-     nn=cube.data.shape[0]
-     ranges=[ranges[2],ranges[3],ranges[4],ranges[5],-nn/2,nn/2]
-     #nn=cube.data.shape[0]
-     #vect=np.linspace(0.0,1.0,nn)
-     #vfield=np.average(cube.data,axis=0,weights=vect)
-     rms=cube.estimate_rms()
-     afield=np.argmax(cube.data,axis=0) - nn/2
-     vfield=np.max(cube.data,axis=0)
-     afield[vfield<1.5*rms]=0
-     afield[afield==-20]=0
-     mlab.surf(afield,warp_scale="auto")
-     ax=mlab.axes(xlabel="DEC [deg]",ylabel="RA [deg]",zlabel="PIX",ranges=ranges,nb_labels=5)
-     ax.axes.label_format='%.3f'
-     mlab.colorbar(title='Pix', orientation='vertical', nb_labels=5)
-     mlab.show()
+#def velocity(cube):
+#     figure = mlab.figure('Velocity Map')
+#     ranges=cube.get_ranges()
+#     nn=cube.data.shape[0]
+#     ranges=[ranges[2],ranges[3],ranges[4],ranges[5],-nn/2,nn/2]
+#     #nn=cube.data.shape[0]
+#     #vect=np.linspace(0.0,1.0,nn)
+#     #vfield=np.average(cube.data,axis=0,weights=vect)
+#     rms=cube.estimate_rms()
+#     afield=np.argmax(cube.data,axis=0) - nn/2
+#     vfield=np.max(cube.data,axis=0)
+#     afield[vfield<1.5*rms]=0
+#     afield[afield==-20]=0
+#     mlab.surf(afield,warp_scale="auto")
+#     ax=mlab.axes(xlabel="DEC [deg]",ylabel="RA [deg]",zlabel="PIX",ranges=ranges,nb_labels=5)
+#     ax.axes.label_format='%.3f'
+#     mlab.colorbar(title='Pix', orientation='vertical', nb_labels=5)
+#     mlab.show()
 
 
 
