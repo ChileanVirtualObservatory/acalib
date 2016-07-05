61d9b69a9e28e02d7d4368b364e7f2d68d5399a1
mavillan <mavillan@alumnos.inf.utfsm.cl>
2016-07-05 12:24:03 -0400

    pycupid working! CAAs has only -1 values
    Former-commit-id: b8770381ec7297a1472afcd0bd2ffa569c731bdb

--- acalib/cupid/cupidsub/pycf.pyx
+++ acalib/cupid/cupidsub/pycf.pyx
@@ -10,29 +10,35 @@ ctypedef cnp.int_t int_t
 ctypedef cnp.float64_t float64_t
 ctypedef cnp.ndarray ndarray
 
-cdef ndarray _test():
+cdef int[:] _test():
 	#clumpf( int type, int ndim, int *slbnd, int *subnd, 
 	#void *ipd, double *ipv, double rms, AstKeyMap *config, int velax, 
 	#int perspectrum, double beamcorr[ 3 ], int *backoff, int *status )
 	cdef:
-		int *slbnd
-		int *subnd
-		double *beamcorr
+		int *slbnd = [0,0,0]
+		int *subnd = [10,10,10]
+		double *beamcorr = [0.,0.,0.]
 		int backoff = 0
-		int status = 1
+		int status = 0
 		ndarray[float64_t, ndim=1, mode='fortran'] inp_arr
-		int [::1] out_arr
-		AstKeyMap *kmap 
-	slbnd[:] = [0,0,0]
-	subnd[:] = [10,10,10]
-	beamcorr[:] = [0.,0.,0.]
+		int [:] out_arr
+		#AstKeyMap *kmap
+		#PyObject *kmap
+		#dict Dict
+	tmp = dict()
+	cdef PyObject* kmap = <PyObject *> tmp
+	#slbnd[:] = [0,0,0]
+	#subnd[:] = [10,10,10]
+	#beamcorr[:] = [0.,0.,0.]
 	inp_arr = np.random.random((10,10,10)).flatten(order='F')
-	out_arr = <int[:1]> cupidClumpFind(1, 3, slbnd, subnd, <double*> inp_arr.data, NULL, 1., kmap, 0, 0, beamcorr, &backoff, &status)
-	#out_arr = np.array(out_arr)
+	out_arr = <int[:1000]> cupidClumpFind(1, 3, slbnd, subnd, <double*> inp_arr.data, NULL, 0.05, kmap, 0, 0, beamcorr, &backoff, &status)
+	#out_arr = .asarray(out_arr)
 	return out_arr
 
 def sum(a,b):
 	return a+b
 
 def test():
-	return _test()
+	mv = _test()
+	return mv 
+	#return np.asarray(_test())
