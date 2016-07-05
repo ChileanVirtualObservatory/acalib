1bc6f40781d1c30b08c95b2d6adb0aebab8f443a
mavillan <mavillan@alumnos.inf.utfsm.cl>
2016-07-05 08:19:01 -0400

    1)Fixed problem with ndarray casting. 2)Added readme for compilation
    Former-commit-id: 5d989bf38031681cb31a0217d470f5b06d918db3

--- acalib/cupid/cupidsub/pycf.pyx
+++ acalib/cupid/cupidsub/pycf.pyx
@@ -2,22 +2,31 @@
 
 import numpy as np
 cimport numpy as cnp
-from clumpfind cimport cupidClumpFind
+from clumpfind cimport AstKeyMap, cupidClumpFind
 
+
+ctypedef cnp.int_t int_t
 ctypedef cnp.float64_t float64_t
+ctypedef cnp.ndarray ndarray
 
 cdef float64_t[::1] _test():
 	#clumpf( int type, int ndim, int *slbnd, int *subnd, 
 	#void *ipd, double *ipv, double rms, AstKeyMap *config, int velax, 
 	#int perspectrum, double beamcorr[ 3 ], int *backoff, int *status )
 	cdef:
-		int *slbnd = [0,0,0]
-		int *subnd = [10,10,10]
-		double *beamcorr = [0,0,0]
-		int *backoff = [0]
-		int *status = [1]
+		int *slbnd
+		int *subnd
+		double *beamcorr
+		int backoff = 0
+		int status = 1
+		ndarray[float64_t, ndim=3, mode='fortran'] arr
+		AstKeyMap kmap 
+	#slbnd[:] = [0,0,0]
+	#subnd[:] = [10,10,10]
+	#beamcorr = [0.,0.,0.]
 	arr = np.random.random((10,10,10)).flatten(order='F')
-	arr2 = cupidClumpFind(1, 3, slbnd, subnd, <double*> arr.data, NULL, 1., dict(), 0, 0, beamcorr, backoff, status)
+	beamcorr = <double*> arr.data
+	arr2 = cupidClumpFind(1, 3, slbnd, subnd, <double*> arr.data, NULL, 1., &kmap, 0, 0, beamcorr, &backoff, &status)
 	return arr
 
 def sum(a,b):
