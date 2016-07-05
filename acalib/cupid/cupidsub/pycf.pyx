8cfc2100522c20f11e39724915f0e77a8bb9a788
mavillan <mavillan@alumnos.inf.utfsm.cl>
2016-07-05 10:39:13 -0400

    last advances over pycf.pyx
    Former-commit-id: d1ad80ef3e69ed78a56fc1500322f12864a9d29a

--- acalib/cupid/cupidsub/pycf.pyx
+++ acalib/cupid/cupidsub/pycf.pyx
@@ -10,7 +10,7 @@ ctypedef cnp.int_t int_t
 ctypedef cnp.float64_t float64_t
 ctypedef cnp.ndarray ndarray
 
-cdef int_t[::1] _test():
+cdef ndarray _test():
 	#clumpf( int type, int ndim, int *slbnd, int *subnd, 
 	#void *ipd, double *ipv, double rms, AstKeyMap *config, int velax, 
 	#int perspectrum, double beamcorr[ 3 ], int *backoff, int *status )
@@ -21,13 +21,14 @@ cdef int_t[::1] _test():
 		int backoff = 0
 		int status = 1
 		ndarray[float64_t, ndim=1, mode='fortran'] inp_arr
-		int *out_arr
+		int [::1] out_arr
 		AstKeyMap *kmap 
-	#slbnd[:] = [0,0,0]
-	#subnd[:] = [10,10,10]
-	#beamcorr = [0.,0.,0.]
+	slbnd[:] = [0,0,0]
+	subnd[:] = [10,10,10]
+	beamcorr[:] = [0.,0.,0.]
 	inp_arr = np.random.random((10,10,10)).flatten(order='F')
-	out_arr = cupidClumpFind(1, 3, slbnd, subnd, <double*> inp_arr.data, NULL, 1., kmap, 0, 0, beamcorr, &backoff, &status)
+	out_arr = <int[:1]> cupidClumpFind(1, 3, slbnd, subnd, <double*> inp_arr.data, NULL, 1., kmap, 0, 0, beamcorr, &backoff, &status)
+	#out_arr = np.array(out_arr)
 	return out_arr
 
 def sum(a,b):
