80ec26a24c409bcbe59917184da06b5535296a37
Mauricio Araya <maray@inf.utfsm.cl>
2016-07-13 18:16:49 -0400

    This branch is good

--- acalib/__init__.py
+++ acalib/__init__.py
@@ -1,12 +1,18 @@
 from . import core
 from .core import *
 
+<<<<<<< HEAD
+import warnings
+from astropy.utils.exceptions import AstropyWarning
+warnings.simplefilter('ignore', category=AstropyWarning)
+=======
 from . import algorithms
 
 import warnings
 from astropy.utils.exceptions import AstropyWarning
 warnings.simplefilter('ignore', category=AstropyWarning)
 warnings.filterwarnings('ignore', category=UserWarning, append=True)
+>>>>>>> f212972b038469446ffa48c5d21e86f18c311105
 
 #from __future__ import absolute_import, print_function
 
