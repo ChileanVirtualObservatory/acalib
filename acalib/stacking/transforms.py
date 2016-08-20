a6c138bfb958c6b65d8422e63f571716bbb5e42d
vansimonsen <vansimonsen@gmail.com>
2016-08-19 21:51:03 -0300

    a little fix in stacking

--- acalib/stacking/transforms.py
+++ acalib/stacking/transforms.py
@@ -13,24 +13,29 @@ def scale(inputDir, majorAxisTemplate):
 	data = glob.glob(inputDir+'/*.fits')
 	scaledData = []
 
+	print data
+
 	for i in np.arange(len(data)):
 	    prop = pr.fits_props(data[i])
+	    print prop
 	    scale = majorAxisTemplate/prop['major']
 	    scaledData.append(scnd.zoom(prop['orig'],scale))
-	    
+	
+
 	return scaledData
 
 
 
 def rotate(data, templateAngle):
 	rotatedData = []
+	angles = []
 
 	for i in np.arange(len(data)):
 	    prop = pr.img_props(data[i])
-	    angle  = templateAngle - prop['angle']
-	    rotatedData.append(scnd.rotate(arrays[i], angle, reshape = True))
+	    angles.append(templateAngle - prop['angle'])
+	    rotatedData.append(scnd.rotate(data[i], angles[-1], reshape = True))
 	
-	return rotatedData	
+	return rotatedData, angles	
 
 
 
@@ -47,18 +52,19 @@ def limits(img,angle):
     return upper,lower
     
     
-def cropAndAlign(data,angle):
+def cropAndAlign(data,angles):
 	alignedData = []
 	shapes = []
 
 	for i in np.arange(len(data)):
-		upper, lower = limits(data[i],angle)
+		upper, lower = limits(data[i],angles[i])
 		crop = data[i][upper[1]:lower[1], upper[1]:lower[1]]
 		shapes.append(list(crop.shape))
 
 		alignedData.append(crop)
 
 	minShape = tuple(np.amin(shapes,axis = 0))
+	
 
 	for i in np.arange(len(alignedData)):
 		dxl = (alignedData[i].shape[0] - minShape[0])/2
@@ -69,3 +75,4 @@ def cropAndAlign(data,angle):
 		alignedData[i] = alignedData[i][dxl:dxr, dyu:dyd]
 
 	return alignedData
+
