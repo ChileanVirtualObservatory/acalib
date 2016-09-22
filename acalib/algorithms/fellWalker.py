dd158e5097dfe0269b9fd5abcc4661d043143abf
mavillan <mavillan@alumnos.inf.utfsm.cl>
2016-09-22 12:39:16 -0300

    fixed indentation and minor errors
    Former-commit-id: e9566affbe1119cc3264339bc7586385f4f63d15

--- acalib/algorithms/fellWalker.py
+++ acalib/algorithms/fellWalker.py
@@ -35,25 +35,25 @@ def _struct_builder(caa):
 
 @support_nddata
 def _fellwalker(data, config, wcs=None, mask=None, unit=None, rms=0.0):
-	data = data.astype(np.float64)
-	ret = pycupid.fellwalker(data, config, rms)
-	return NDData(ret, uncertainty=None, mask=None, wcs=wcs, meta=None, unit=unit)
+    data = data.astype(np.float64)
+    ret = pycupid.fellwalker(data, config, rms)
+    return NDData(ret, uncertainty=None, mask=None, wcs=wcs, meta=None, unit=unit)
 
 
-class FellWalker(Algorithn):
+class FellWalker(Algorithm):
 
-	def default_params(self):
-		if 'FWHMBEAM' not in self.config:
-			self.config['FWHMBEAM'] = 2.0
-		if 'VELORES' not in self.config:
-			self.config['VELORES'] =  2.0  
+    def default_params(self):
+        if 'FWHMBEAM' not in self.config:
+            self.config['FWHMBEAM'] = 2.0
+        if 'VELORES' not in self.config:
+            self.config['VELORES'] =  2.0  
 
-	def run(self, data):
-		# if rms not in config, estimate it
+    def run(self, data):
+        # if rms not in config, estimate it
         if 'RMS' not in self.config:
             rms = acalib.rms(data)
         else:
-        	rms = self.config['RMS']
+            rms = self.config['RMS']
 
         # computing the CAA through CUPID's fellwalker clumping algorithm
         caa = _fellwalker(data, self.config, rms=rms)
