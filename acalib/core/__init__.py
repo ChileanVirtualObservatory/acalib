82e805c69de67a5d99d6566f9c2afb836c17dab4
Mauricio Araya <maray@inf.utfsm.cl>
2016-10-18 01:45:35 +0200

    Fix init and remove useless stuff

--- acalib/core/__init__.py
+++ acalib/core/__init__.py
@@ -1,27 +1,6 @@
-from . import container
-from . import indices
-from . import utils
-
-#ROI Segmentation
-from . import cube_spectra
-from . import gaussian_segmentation
-from . import vel_stacking
-from . import statistics
-
-#Stacking
-from . import props
-from . import transforms
-from . import stack
-
 from .container import *
-from .indices import *
-from .utils import *
-
-from .cube_spectra import *
-from .gaussian_segmentation import *
-from .vel_stacking import *
-from .statistics import *
-
-from .props import *
-from .stack import *
-from .transforms import *
+from .analysis import *
+from .transform import *
+from .axes import *
+from .flux import *
+from .models import *
