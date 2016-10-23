39abf6c0be239ecda5305fe8bcb5244c0b9a8eef
Mauricio Araya <maray@inf.utfsm.cl>
2016-10-23 13:48:28 +0200

    Indexing executing, not working.
    Former-commit-id: 61ccca249dba09795aeaa3e37457e0aa3a0d8d72

--- acalib/algorithms/indexing.py
+++ acalib/algorithms/indexing.py
@@ -1,49 +1,48 @@
 import acalib
 from .algorithm import Algorithm
 
+
 class Indexing(Algorithm):
-	def default_params(self):
-		if 'P' not in self.config:
-			self.config['P'] = 0.05
-		if 'PRECISION' not in self.config:
-			self.config['PRECISION'] = 0.02
-		if 'RANDOM_STATE' not in self.config:
-			self.config['RANDOM_STATE'] = None
-		if 'SAMPLES' not in self.config:
-			self.config["SAMPLES"]  = 1000
+    def default_params(self):
+        if 'P' not in self.config:
+            self.config['P'] = 0.05
+        if 'PRECISION' not in self.config:
+            self.config['PRECISION'] = 0.02
+        if 'RANDOM_STATE' not in self.config:
+            self.config['RANDOM_STATE'] = None
+        if 'SAMPLES' not in self.config:
+            self.config["SAMPLES"] = 1000
 
-	def run(self,data): 
+    def run(self, data):
 
-		if data.wcs:
-			wcs = data.wcs
-		else:
-			wcs = None
+        if data.wcs:
+            wcs = data.wcs
+        else:
+            wcs = None
 
-		c = acalib.Container()
+        c = acalib.Container()
 
-		spectra, slices = acalib.cube_spectra(data,self.config["SAMPLES"],self.config["RANDOM_STATE"])
+        spectra, slices = acalib.core.spectra_sketch(data.data, self.config["SAMPLES"], self.config["RANDOM_STATE"])
 
-		pp_slices = []
-		for slice in slices:
-			pp_slice  = acalib.vel_stacking(data, slice)
-			labeled_images = acalib.gaussian_mix(pp_slice,prob=self.config["P"], precision=self.config["PRECISION"])
+        pp_slices = []
+        for slice in slices:
+            pp_slice = acalib.core.vel_stacking(data, slice)
+            labeled_images = acalib.core.gaussian_mix(pp_slice, prob=self.config["P"],
+                                                      precision=self.config["PRECISION"])
 
-			if wcs is not None:
-				freq_min = float(wcs.all_pix2world(0,0,slice.start,1)[2])
-				freq_max = float(wcs.all_pix2world(0,0,slice.stop,1)[2])
-			else:
-				freq_min = None
-				freq_max = None
+            if wcs is not None:
+                freq_min = float(wcs.all_pix2world(0, 0, slice.start, 1)[2])
+                freq_max = float(wcs.all_pix2world(0, 0, slice.stop, 1)[2])
+            else:
+                freq_min = None
+                freq_max = None
 
-			table = acalib.measure_shape(pp_slice, labeled_images, freq_min , freq_max )
-			if len(table) > 0:
-				c.tables.append(table)
-				c.images.append(pp_slice)
-				c.images.extend(labeled_images)
+            table = acalib.core.measure_shape(pp_slice, labeled_images, freq_min, freq_max)
+            if len(table) > 0:
+                c.tables.append(table)
+                c.images.append(pp_slice)
+                c.images.extend(labeled_images)
 
-
-		c.images.insert(0,data) 
-		c.primary = c.images[0]
-		return c
-
-
+        c.images.insert(0, data)
+        c.primary = c.images[0]
+        return c
