b2f012355480ca151370b8534efffa076fa51752
Mauricio Araya <maray@inf.utfsm.cl>
2016-10-24 14:10:19 +0200

    Fix indexing for the new core
    Former-commit-id: bb39d37b734e2aef4aad31da4288acd782771c04

--- acalib/algorithms/indexing.py
+++ acalib/algorithms/indexing.py
@@ -3,6 +3,7 @@ from .algorithm import Algorithm
 
 
 class Indexing(Algorithm):
+
     def default_params(self):
         if 'P' not in self.config:
             self.config['P'] = 0.05
@@ -26,7 +27,7 @@ class Indexing(Algorithm):
 
         pp_slices = []
         for slice in slices:
-            pp_slice = acalib.core.vel_stacking(data, slice)
+            pp_slice = acalib.core.vel_stacking(data.data, slice)
             labeled_images = acalib.core.gaussian_mix(pp_slice, prob=self.config["P"],
                                                       precision=self.config["PRECISION"])
 
@@ -40,9 +41,12 @@ class Indexing(Algorithm):
             table = acalib.core.measure_shape(pp_slice, labeled_images, freq_min, freq_max)
             if len(table) > 0:
                 c.tables.append(table)
+                print("pp")
                 c.images.append(pp_slice)
+                print(type(pp_slice))
+                print("lb")
                 c.images.extend(labeled_images)
-
+                print(type(labeled_images))
         c.images.insert(0, data)
         c.primary = c.images[0]
         return c
