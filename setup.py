0e470bab8d67b51d5273b4a2565f09b8f5db8525
Camilo Valenzuela <cacke91@gmail.com>
2016-11-03 14:13:26 -0300

    Changed version on setup.py

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def setup_package():
 
     setup(
         name = "acalib",
-        version = "1.0.0",
+        version = "0.1.0",
         description = "Advanced Computing for Astronomy Library",
         url = "https://github.com/ChileanVirtualObservatory/ACALIB",
         author = "LIRAE",
