b5a8f2b5c78be4375f641b9947b8771db898db17
Vansimonsen <vansimonsen@gmail.com>
2016-12-15 17:17:32 -0300

    flux documented

--- acalib/upi/flux.py
+++ acalib/upi/flux.py
@@ -6,8 +6,24 @@ from acalib.upi.axes import opening, features, axes_units
 
 @support_nddata
 def noise_level(data,mask=None,unit=None):
-    """Compute the RMS of data.
     """
+        Compute the RMS of data.
+
+        Parameters
+        ----------
+        data: (M,N) numpy.ndarray
+        mask: numpy.ndarray
+            mask for the data
+        unit: astropy.units.Unit
+            Astropy Unit (http://docs.astropy.org/en/stable/units/)
+
+        Returns
+        -------
+        rms: float 
+            RMS of data
+    """
+
+
     #TODO: check photutils background estimation for using that if possible
     if unit is None:
         return core.rms(data,mask)
@@ -16,7 +32,23 @@ def noise_level(data,mask=None,unit=None):
 
 @support_nddata
 def standarize(data, wcs=None, unit=None, mask=None, meta=None):
-    """ Standarize data: data = a * res + b"""
+    """
+        Standarize data: 
+
+        Parameters
+        ----------
+        data: (M,N) numpy.ndarray
+        wcs: World Coordinate System data (http://docs.astropy.org/en/stable/wcs/)
+        mask: numpy.ndarray
+            mask for the data
+        unit: astropy.units.Unit
+            Astropy Unit (http://docs.astropy.org/en/stable/units/)
+        meta: FITS metadata
+
+        Returns
+        -------
+        Standarized data where data = a * res + b
+    """
     if mask is not None:
         data = core.fix_mask(data, mask)
     (res, a, b) = core.standarize(data)
@@ -26,7 +58,28 @@ def standarize(data, wcs=None, unit=None, mask=None, meta=None):
 
 @support_nddata
 def unstandarize(data, a, b, wcs=None, unit=None, mask=None, meta=None):
-    """ unstandarize data: res = a * data + b"""
+    """
+        Unstandarize data: res = a * data + b
+        
+
+        Parameters
+        ----------
+        data: (M,N) numpy.ndarray
+        a: float
+            slope of straight
+        b: float
+            Intercept of straight
+        wcs: World Coordinate System data (http://docs.astropy.org/en/stable/wcs/)
+        mask: numpy.ndarray
+            mask for the data
+        unit: astropy.units.Unit
+            Astropy Unit (http://docs.astropy.org/en/stable/units/)
+        meta: FITS metadata
+
+        Returns
+        -------
+        Unstandarized data: res = a * data + b
+    """
     if mask is not None:
         data = core.fix_mask(data, mask)
     res = core.unstandarize(data, a, b)
@@ -36,11 +89,33 @@ def unstandarize(data, a, b, wcs=None, unit=None, mask=None, meta=None):
 
 @support_nddata
 def add(data, flux, lower=None, upper=None,wcs=None,unit=None,meta=None,mask=None):
-    """ Create a new data with the new flux added. 
+    """ 
+        Create a new data with the new flux added. 
 
-    Lower and upper are bounds for data. This operation is border-safe and creates a new object at each call.
-    Please use the OO version data.add(flux) for modifying the data itself.
+        Lower and upper are bounds for data. This operation is border-safe and creates a new object at each call.
+
+        Parameters
+        ----------
+        data: (M,N) numpy.ndarray
+        flux: float
+            Flux of data
+        lower: numpy.ndarray
+        upper: numpy.ndarray
+            Bounds for data
+        wcs: World Coordinate System data (http://docs.astropy.org/en/stable/wcs/)
+        mask: numpy.ndarray
+            mask for the data
+        unit: astropy.units.Unit
+            Astropy Unit (http://docs.astropy.org/en/stable/units/)
+        meta: FITS metadata
+
+        Returns
+        -------
+        NDData structure with new flux added
+    
     """
+
+    #Please use the OO version data.add(flux) for modifying the data itself.
     res = data.copy()
     core.add(res, flux, lower, upper)
     return NDData(res, uncertainty=None, mask=mask, wcs=wcs, meta=None, unit=unit)
@@ -48,15 +123,52 @@ def add(data, flux, lower=None, upper=None,wcs=None,unit=None,meta=None,mask=Non
 
 @support_nddata
 def denoise(data, wcs=None, mask=None, unit=None, threshold=0.0):
-    """ Simple denoising given a threshold (creates a new object) """
+    """
+        Simple denoising given a threshold (creates a new object) 
+
+        Parameters
+        ----------
+        data: (M,N) numpy.ndarray
+        wcs: World Coordinate System data (http://docs.astropy.org/en/stable/wcs/)
+        mask: numpy.ndarray
+            mask for the data
+        unit: astropy.units.Unit
+            Astropy Unit (http://docs.astropy.org/en/stable/units/)
+        threshold: float
+
+        Returns
+        -------
+        Data denoised
+        -------        
+    """
     newdata = core.denoise(data, threshold.value)
     return NDData(newdata, uncertainty=None, mask=mask, wcs=wcs, meta=None, unit=unit)
 
 
 @support_nddata
 def world_gaussian(data, mu, P, peak, cutoff, wcs=None):
-    """ Creates a gaussian flux at mu position (WCS), with P shape, with a maximum value equal to peak,
-    and with compact support up to the cutoff contour """
+    """ 
+        Creates a gaussian flux at mu position (WCS), with P shape, with a maximum value equal to peak,
+        and with compact support up to the cutoff contour
+
+        Parameters
+        ----------
+        data: (M,N) numpy.ndarray
+        mu: float
+        P: tuple
+            Shape of result
+        peak: float
+            maximum value
+        cutoff:
+
+        wcs: World Coordinate System data (http://docs.astropy.org/en/stable/wcs/)
+
+        Returns
+        -------
+        
+        Tuple of gaussian flux and borders
+    
+    """
     Sigma = np.linalg.inv(P)
     window = np.sqrt(2 * np.log(peak / cutoff) * np.diag(Sigma)) * axes_units(data, wcs=wcs)
     lower, upper = opening(data, mu, window, wcs=wcs)
