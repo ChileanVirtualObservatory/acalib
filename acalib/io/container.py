bcf900b75d7a14a91d413e68e378b8ea5f766cf5
Manuel Sanchez <manuel.sanchez@linux.com>
2016-12-16 01:36:51 -0300

    Changed every instance of the word 'fits' to its capitalized version, 'FITS'
    Former-commit-id: 9bc839babb8444e29d3fc45c8d03666e23ea8e46

--- acalib/io/container.py
+++ acalib/io/container.py
@@ -3,9 +3,10 @@ from astropy import log
 from acalib.io.fits import save_fits_from_cont,load_fits_to_cont
 
 class Container:
-    """ Data structure that contains a list of NDData and astropy tables.
+    """
+    Data structure that contains a list of NDData and astropy tables.
     
-    It can load from and save to fits file format. To write a fits, please put in "primary"
+    It can load from and save to FITS file format. To write a FITS, please put in "primary"
     the main object, and in nndata and table all the extensions.
     """
     def __init__(self): 
@@ -24,16 +25,16 @@ class Container:
 
 def load_fits(path):
     """
-    Load a fits into a container.
+    Load a FITS into a container.
 
     Parameters
     ----------
     path : str
-        Path to fits file in local disk.
+        Path to FITS file in local disk.
 
     Returns
     -------
-    result: :class:`~acalib.Container` with the fits loaded.
+    result: :class:`~acalib.Container` with the FITS loaded.
     """
     cont=Container()
     cont.load_fits(path)
@@ -41,13 +42,13 @@ def load_fits(path):
 
 def save_fits(cont,path):
     """
-    Save a fits from a container.
+    Create a new FITS file from a container.
 
     Parameters
     ----------
     cont : :class:`~acalib.Container`
 
     path : str
-    	Path to new fits file to be created from the container.
+    	Path to new FITS file to be created from the container.
     """
     cont.save_fits(path)
