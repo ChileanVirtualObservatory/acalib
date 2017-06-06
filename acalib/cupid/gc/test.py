801962f4562a61dfd5103659074aaec9a2fb1687
Mauricio Araya <maray@inf.utfsm.cl>
2017-06-06 14:43:08 -0400

    Remove gc

--- acalib/cupid/gc/test.py
+++
@@ -1,8 +0,0 @@
-import pycupid
-import numpy as np
-d = np.array([1,2,3], dtype=np.double)
-v = np.array([0,0,0], dtype=np.double)
-cfg = {"answer": 42, "pi": np.pi, "question": "the answer to life the universe and everything"}
-rms = 30
-velax = 0
-pycupid.gaussclumps(d,v,cfg,rms,velax)
