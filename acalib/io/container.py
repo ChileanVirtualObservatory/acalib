f95d861904e2b801e39a6d274a170d9d263939d2
Camilo Valenzuela <cacke91@gmail.com>
2017-07-12 16:44:44 -0400

    Removed container from indexing, changed for namedtuple

--- acalib/io/container.py
+++ acalib/io/container.py
@@ -5,22 +5,22 @@ from acalib.io.fits import save_fits_from_cont,load_fits_to_cont
 class Container:
     """
     Data structure that contains a list of NDData and astropy tables.
-    
+
     It can load from and save to FITS file format. To write a FITS, please put in "primary"
     the main object, and in nndata and table all the extensions.
     """
-    def __init__(self): 
+    def __init__(self):
         self.primary = None
-        """Primary object""" 
-        self.images = [] 
-        """List of NDData object""" 
+        """Primary object"""
+        self.images = []
+        """List of NDData object"""
         self.tables = []
-        """List of astropy tables""" 
+        """List of astropy tables"""
 
-    def load_fits(self,path): 
+    def load_fits(self,path):
         load_fits_to_cont(path,self)
-    def save_fits(self,path): 
-        save_fits_from_cont(path,self) 
+    def save_fits(self,path):
+        save_fits_from_cont(path,self)
 
 
 def load_fits(path):
