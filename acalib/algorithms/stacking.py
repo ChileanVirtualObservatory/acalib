424e459048bd41bf11d7be39629fdb8393e4ddff
Autopawn <franciscojacb@gmail.com>
2017-07-13 16:53:56 -0400

    Replaced stacking algorithm.

--- acalib/algorithms/stacking.py
+++ acalib/algorithms/stacking.py
@@ -1,10 +1,15 @@
 import acalib
 from .algorithm import Algorithm
 
+import numpy
 from numpy import mean
 from scipy.stats import signaltonoise
 import scipy.ndimage as scnd
 from astropy.nddata import NDData,NDDataRef
+
+import skimage
+
+import matplotlib.pyplot as plt
 
 class Stacking(Algorithm):
     """
@@ -14,7 +19,7 @@ class Stacking(Algorithm):
     def default_params(self):
         pass
 
-    def run(self, template_data, images):
+    def run(self, template_data, images, display_tranformations=False):
         """
             Run the stacking algorithm given a template image and a container of images.
 
@@ -37,20 +42,69 @@ class Stacking(Algorithm):
             if type(images[i]) is not NDData or type(images[i]) is not NDDataRef:
                 images[i] = NDDataRef(images[i])
 
+        template_data = numpy.copy(template_data)
         tprops = acalib.core.transform.fits_props(template_data)
 
-        # TODO: Replace with core.transform.scale once it stops using
-        # a acalib.container.
-        majorAxisTemplate = tprops['major']
-        scaledData = []
+        order = 1
         for i in range(len(images)):
-            prop = acalib.core.transform.fits_props(images[i].data)
-            sc = majorAxisTemplate / prop['major']
-            scaledData.append(scnd.zoom(prop['orig'], sc))
-        scaled = scaledData
+            data1 = images[i].data
+            props = acalib.core.transform.fits_props(data1)
 
-        rotated, angles = acalib.core.transform.rotate(scaled, tprops['angle'])
-        aligned = acalib.core.transform.crop_and_align(rotated, angles)
-        result = mean(aligned,axis=0)
+            def blit_add(dest, src, loc):
+                str_y = max(0,loc[0])
+                str_x = max(0,loc[1])
+                end_y = min(dest.shape[0],loc[0]+src.shape[0])
+                end_x = min(dest.shape[1],loc[1]+src.shape[1])
+                str_y_2 = max(0,-loc[0])
+                str_x_2 = max(0,-loc[1])
+                end_y_2 = str_y_2+end_y-str_y
+                end_x_2 = str_x_2+end_x-str_x
+                dest[str_y:end_y,str_x:end_x] += src[str_y_2:end_y_2,str_x_2:end_x_2]
+                return dest
 
-        return result
+            # Extend matrix so that the centroid is at the center of the image.
+            max_img_dim = max(data1.shape[0]*2,data1.shape[1]*2)
+            data2 = numpy.zeros((max_img_dim,max_img_dim))
+            cy = data2.shape[0]//2-int(numpy.round(props['centroid'][0]))
+            cx = data2.shape[1]//2-int(numpy.round(props['centroid'][1]))
+            data2[cy:cy+data1.shape[0],cx:cx+data1.shape[1]] = data1
+
+            # Rotate the image so that the larger radious is at 0 degrees:
+            data3 = skimage.transform.rotate(data2,
+                numpy.rad2deg(-props['angle']),cval=0,order=order)
+
+            # Rescale the image so that it meets the shape of the template:
+            ysize = int(numpy.round(
+                data3.shape[0]*tprops['ratio']/props['ratio']))
+            data4 = skimage.transform.resize(data3,(ysize,data3.shape[1]),
+                order=order)
+            # Extend matrix so that the centroid is at the center of the image.
+            data5 = numpy.zeros(data2.shape)
+            cy = (data3.shape[0]-data4.shape[0])//2
+            blit_add(data5,data4,(cy,0))
+
+            # Rotate image again, now to match the angle of the template:
+            data6 = skimage.transform.rotate(data5,
+                numpy.rad2deg(tprops['angle']),cval=0,order=order)
+
+            # Scale the image:
+            main_ratio = tprops['major']/props['major']
+            data7 = skimage.transform.rescale(data6,main_ratio,
+                order=order,cval=0, clip=False)
+
+            delta_y = int(numpy.round(tprops['centroid'][0]-data7.shape[0]//2))
+            delta_x = int(numpy.round(tprops['centroid'][1]-data7.shape[1]//2))
+            blit_add(template_data,data7,(delta_y,delta_x))
+
+            # Display partial results:
+            if display_tranformations:
+                f, axarr = plt.subplots(3,2,figsize=(10,10))
+                axarr[0,0].imshow(data2,cmap='gray')
+                axarr[0,1].imshow(data3,cmap='gray')
+                axarr[1,0].imshow(data5,cmap='gray')
+                axarr[1,1].imshow(data6,cmap='gray')
+                axarr[2,0].imshow(data7,cmap='gray')
+                axarr[2,1].imshow(template_data,cmap='gray')
+                plt.show()
+
+        return template_data
