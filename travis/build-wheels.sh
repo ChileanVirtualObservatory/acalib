efb36873b435e0a313291c7433215578c27081ee
Camilo Valenzuela <cacke91@gmail.com>
2017-07-12 20:36:32 -0400

    Changed workingdir folder for travis

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -3,17 +3,17 @@ set -e -x
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /workspace/requirements.txt
-    "${PYBIN}/pip" wheel /workspace/ -w wheelhouse/
+    "${PYBIN}/pip" install -r /io/requirements.txt
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
-    auditwheel repair "$whl" -w /workspace/wheelhouse/
+    auditwheel repair "$whl" -w /io/wheelhouse/
 done
 
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /workspace/wheelhouse
-    (cd "$HOME"; "${PYBIN}/python" /workspace/testing/run_test.py)
+    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
+    (cd "$HOME"; "${PYBIN}/python" /io/testing/run_test.py)
 done
