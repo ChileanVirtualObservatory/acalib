e38d4c322a55e9de905b89d47df0e484c57da1fe
Camilo Valenzuela <cacke91@gmail.com>
2017-07-12 21:25:07 -0400

    Added some requirements to build script

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e -x
 VERSIONS=(cp27-cp27mu cp34-cp34m cp35-cp35m cp36-cp36m)
+
+yum update -y
+yum groupinstall -y "Development Tools"
+yum install -y csh libXext-devel libXau-devel libX11-devel libXt-devel libxml2-devel ncurses-devel texlive-multirow python-devel Cython
+ 
 # Compile wheels
 for PYBIN in ${VERSIONS[@]}; do
-    PYBIN=/opt/python/$PYBIN/bin/ 
+    PYBIN=/opt/python/$PYBIN/bin/
     "${PYBIN}/pip" install -r /io/requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
@@ -13,7 +18,7 @@ for whl in wheelhouse/*.whl; do
     auditwheel repair "$whl" -w /io/wheelhouse/
 done
 
-for PYBIN in ${VERSIONS[@]}; do    
+for PYBIN in ${VERSIONS[@]}; do
     PYBIN=/opt/python/$PYBIN/bin/
     "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
     (cd "$HOME"; "${PYBIN}/python" /io/testing/run_test.py)
