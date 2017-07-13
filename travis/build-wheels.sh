5401b5ac4addd08b64e15f6734bf69f1946e1138
Camilo Valenzuela <cacke91@gmail.com>
2017-07-12 21:07:08 -0400

    Added python versions

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e -x
-
+VERSIONS=(cp27-cp27m cp27-cp27mu cp33-cp33m cp34-cp34m cp35-cp35m cp36-cp36m)
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
+for PYBIN in ${VERSIONS[@]}; do
+    PYBIN=/opt/python/$PYBIN/bin/ 
     "${PYBIN}/pip" install -r /io/requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
@@ -12,8 +13,8 @@ for whl in wheelhouse/*.whl; do
     auditwheel repair "$whl" -w /io/wheelhouse/
 done
 
-# Install packages and test
-for PYBIN in /opt/python/*/bin/; do
+for PYBIN in ${VERSIONS[@]}; do    
+    PYBIN=/opt/python/$PYBIN/bin/
     "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
     (cd "$HOME"; "${PYBIN}/python" /io/testing/run_test.py)
 done
